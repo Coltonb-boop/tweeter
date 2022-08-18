c354b2cd7d7dfbb21584c2082b37895b310e9b45
Coltonb-boop <coltonbitz@gmail.com>
2022-08-18 14:45:16 -0600

    should be mostly complete now

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,4 @@
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -6,38 +7,50 @@
 
 $(() => {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+  $('#new-tweet-container').submit( function(e) {
+    e.preventDefault();
+    const value = $('#tweet-text').val();
+    
+    if (!value) {
+      // alert('You didn\'t write a tweet');
+      $('.complaint-department').hide();
+      $('.complaint-department').text('You didn\'t write a tweet');
+      $('.complaint-department').show(500);
+      return;
     }
-  ]
+    if (value.length > 140) {
+      // alert('You had too much to say!');
+      $('.complaint-department').hide();
+      $('.complaint-department').text('You had too much to say!');
+      $('.complaint-department').show(500);
+      return;
+    }
+    
+    const formData = $('#new-tweet-container').serialize();
+    $('.complaint-department').hide();
+    $( ".counter" ).val(140);
+    $('#tweet-text').val('');
+    $.post('/tweets', formData, () => {
+      loadTweets();
+    });
+  });
+
+  const loadTweets = () => {
+    $.get('http://localhost:8080/tweets')
+      .then(function (tweets) {
+        $('#tweets-container').html('');
+        renderTweets(tweets);
+    });
+  };
+  loadTweets();
 
   const renderTweets = (tweets) => {
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
 
-      $('#tweets-container').append($tweet);
+      $('#tweets-container').prepend($tweet);
     }
-  }
+  };
 
   const createTweetElement = (tweetData) => {
     const $newTweet = $(`
@@ -57,12 +70,12 @@ $(() => {
         </header>
 
         <div class="tweet-body">
-          ${tweetData.content.text}
+          ${escape(tweetData.content.text)}
         </div>
 
         <footer>
           <div>
-            ${tweetData.created_at}
+            ${timeago.format(tweetData.created_at)}
           </div>
           <div class="tweet-footer-icons">
             <i class="fa-solid fa-flag"></i>
@@ -76,5 +89,10 @@ $(() => {
     return $newTweet;
   };
 
-  renderTweets(data);
+  const escape = function (str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
 });
